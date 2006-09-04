113640cb44d72667287878208140dfc089025082
gmiranda <gmiranda@ce2dd6bc-64f8-f6e2-0175-3f3cbbe2768e>
2006-09-04 22:44:49 +0000

    Removed file/folder

--- trunk/src/CImg_plugins.cpp
+++
@@ -1,9 +0,0 @@
-
-/**
- * Replace each image pixel by 10 power by its value.
- * @see log10(), get_log10(), get_pow(), pow()
- */
-CImg& pow10() {
-    cimg_map(*this,ptr,T) (*ptr)=(T)std::pow((double)10.0,(double)(*ptr));
-    return *this;
-}
